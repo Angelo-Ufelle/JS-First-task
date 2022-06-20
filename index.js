d10e4a316338eb5f380237f3c6e3dbf0f1b1a55d
Angelo-Ufelle <onyedikaufelle@gmail.com>
2022-06-20 17:58:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+alert("Welcome to my first JavaScript code")
+document.write("My name is Azubuike-Ufelle Onyedika <br> Height is 5.8ft <br>I am from Nigeria");
+console.log("Azubuike-Ufelle Onyedika")
+console.log("5.8ft")
+console.log("Nigeria")
